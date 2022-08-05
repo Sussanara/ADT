8b2d394f6ba6a124fbb10914b5655126ed44d0e8
Patricio Espinoza <p.alfonsoespinoza@gmail.com>
2022-08-04 20:21:51 -0400

    update home user

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Navbar /> 
-      
+      <Home_User />
       <div className="App"></div>
     </>
   );
--- src/shared_component/home_user.js
+++ src/shared_component/home_user.js
@@ -1,8 +1,52 @@
 import React from "react";
 import "./styles/home_user.css";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { green } from '@mui/material/colors';
+import Icon from '@mui/material/Icon';
 
 export const home_user = () => {
-  return {};
+  const array = [1,2,3,4,5,6,7,8,9,10] 
+    return (
+    < > 
+    <div><h1>Visión General de tu Negocio</h1></div>
+    <div classname="articles">
+        <h3>Artículos</h3>
+        <Icon color="secondary">add_circle</Icon>
+    </div>
+    <div>{array.map((numero)=>{
+        return (
+            <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+        )
+    })}
+    </div>
+    </>
+  )
 };
 
 export default home_user;
