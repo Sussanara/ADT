c1868259ed17b64e24da202b3361825815f73a66
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-08-05 18:15:44 -0400

    GET users, and POST products route is added, more specific url for POSTING products is needed

--- src/app.py
+++ src/app.py
@@ -7,9 +7,34 @@ app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['JSON_SORT_KEYS'] = False
 
 db.init_app(app)
 Migrate(app,db)
+
+@app.route('/api/users', methods = ['GET','POST']) #Acá agregar método POST
+def users_with_products():
+    if request.method == 'GET':
+        users = User.query.all()
+        users = list(map(lambda user: user.serialize_with_products(), users))
+        return jsonify(users),200
+
+
+    if request.method == 'POST':
+        data = request.get_json()
+
+        product = Product()
+        product.owner_id = data['owner_id']
+        product.name = data['name']
+        product.stock = data['stock']
+        product.sold_stock = data['sold_stock']
+        product.price = data['price']
+
+        product.save()
+
+        return jsonify(product.serialize()),200
+        
+
 
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,29 +6,85 @@ db = SQLAlchemy()
 class Admin(db.Model):
     __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(200), nullable = False , unique = True )
+    email = db.Column(db.String(200), nullable = False , unique = True )
     password = db.Column(db.String(200), nullable = False)
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(200), nullable = False, unique = True)
-    password = db.Column(db.String(200) , nullable = False)
     email = db.Column(db.String(200), nullable = False , unique = True)
-    company_name = db.Column(db.String(200))
-    phone = db.Column(db.String(200))
-    firstName = db.Column(db.String(200))
-    lastName = db.Column(db.String(200))
-    rut  = db.Column(db.String(200), nullable = False , unique = True)
+    password = db.Column(db.String(200) , nullable = False)
+    empresa = db.Column(db.String(200), nullable = False , unique = True)
+    phone = db.Column(db.String(200), nullable = False)
+    firstName = db.Column(db.String(200), nullable = False)
+    lastName = db.Column(db.String(200), nullable = False)
+    run  = db.Column(db.String(200), nullable = False , unique = True)
+    is_active = db.Column(db.Boolean, nullable = False, default=True)
     products = db.relationship('Product', backref='user')
+
+    def serialize_with_products(self):
+        return{
+            "id" : self.id,
+            "email" : self.email,
+            "password" : self.password,
+            "empresa" : self.empresa,
+            "phone" : self.phone,
+            "firstName" : self.firstName,
+            "lastName" : self.lastName,
+            "run" : self.run,
+            "is_active": self.is_active,
+            "products": self.get_products()
+        }
+    
+    def get_products(self):
+        return list(map(lambda product: 
+        {
+            "id": product.id,
+            "name": product.name, 
+            "stock": product.stock, 
+            "sold_stock": product.sold_stock,
+            "price": product.price
+        },self.products))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 class Product(db.Model):
     __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key = True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    name = db.Column(db.String(200))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(200), nullable = False)
     stock  = db.Column(db.Integer, nullable = False)
     sold_stock = db.Column(db.Integer, nullable = False)
     price = db.Column(db.Integer, nullable = False)
+
+    def serialize(self):
+        return{
+            "id" : self.id,
+            "owner_id" : self.owner_id,
+            "name" : self.name,
+            "stock" : self.stock,
+            "sold_stock" : self.sold_stock,
+            "price" : self.price
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
