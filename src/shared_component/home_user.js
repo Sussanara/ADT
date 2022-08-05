92801bdc247e51d8b94dd95d0f177e0da391efd8
Patricio Espinoza <p.alfonsoespinoza@gmail.com>
2022-08-05 13:23:23 -0400

    update home user

--- src/shared_component/home_user.js
+++ src/shared_component/home_user.js
@@ -7,9 +7,8 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { green } from "@mui/material/colors";
 import Icon from "@mui/material/Icon";
-import Stack from '@mui/material/Stack';
+import Paper from "@mui/material/Paper";
 
 export const home_user = () => {
   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -19,49 +18,75 @@ export const home_user = () => {
         <h1>Visión General de tu Negocio</h1>
       </div>
       <h3>
-        Artículos <Icon color="primary">add_circle</Icon>
+        Artículos <Icon sx={{ color: "#32CD32", fontSize: 40,   borderBottom: 2  }} color="primary">add_circle</Icon>
       </h3>
       <div>
-        {array.map((numero) => {
+      <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 3,
+          mx: 2,
+          my: 5,
+          width: 270,
+          height: 440,
+        },
+      }}
+    >
+      {array.map((numero) => {
           return (
-            <Card sx={{ maxWidth: 280, maxHeight: 450 ,marginLeft: 2, margin: 2, }}>
-              <CardMedia
-                component="img"
-                height="250"
-                image=""
-                alt=""
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Nombre producto
-                </Typography>
-                <Typography variant="body2" color="text.primary">
-                  Stock: 10
-                </Typography>
-                <Typography variant="body2" color="text.primary">
-                  Vendidas: 15
-                </Typography>
-                <Typography variant="body2" color="text.primary">
-                  valor: $1.000
-                </Typography>
-                <Typography variant="body2" color="text.primary">
-                  Ganancia: $3.000
-                </Typography>
-              </CardContent>
-              <CardActions>
-              <Button sx={{ backgroundColor: "#FF0000", color: "#FFFFFF", marginRight: 1}} variant="contained"  size="small">
-          Eliminar
-          </Button>
-          <Button sx={{ backgroundColor: "#008000", color: "#FFFFFF", marginRight: 1 }} variant="contained" size="small">
-          Ventas
-        </Button>
-        <Button sx={{ backgroundColor: "#9ACD32", color: "#FFFFFF" }} variant="contained" size="small">
-          Editar
-        </Button>
-              </CardActions>
-            </Card>
+            <Paper
+              elevation={8}
+              sx={{ maxWidth: 280, maxHeight: 470, marginLeft: 2, margin: 2 }}
+            >
+              <Card sx={{ maxWidth: 280, maxHeight: 460 }}>
+                <CardMedia component="img" height="240" image="" alt="" />
+
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Nombre producto
+                  </Typography>
+                  <Typography variant="body2" color="text.primary">
+                    Stock: 10
+                  </Typography>
+                  <Typography variant="body2" color="text.primary">
+                    Vendidas: 15
+                  </Typography>
+                  <Typography variant="body2" color="text.primary">
+                    valor: $1.000
+                  </Typography>
+                  <Typography variant="body2" color="text.primary">
+                    Ganancia: $3.000
+                  </Typography>
+                </CardContent>
+                <CardActions sx={{ marginBottom: 2 }}>
+                  <Button
+                    style={{ backgroundColor: "#F44336", color: "white" }}
+                    variant="contained"
+                  >
+                    Eliminar
+                  </Button>
+
+                  <Button
+                    style={{ backgroundColor: "#FF9800", color: "white" }}
+                    variant="contained"
+                  >
+                    Editar
+                  </Button>
+
+                  <Button
+                    style={{ backgroundColor: "#32CD32", color: "white" }}
+                    variant="contained"
+                  >
+                    Ventas
+                  </Button>
+                </CardActions>
+              </Card>
+            </Paper>
           );
         })}
+    </Box>
       </div>
     </>
   );
