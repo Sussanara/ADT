5992b06fa726fc5e257cd9752937384a23c9bbea
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-08-04 22:21:09 -0400

    added database models and flask server running (no routes yet)

--- src/app.py
+++ src/app.py
@@ -1,1 +1,16 @@
-#Aca va la aplicacion en FLASK
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from models import db, Admin, User, Product
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['ENV'] = 'development'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.init_app(app)
+Migrate(app,db)
+
+
+if __name__ == '__main__':
+    app.run()
--- src/models.py
+++ src/models.py
@@ -1,1 +1,34 @@
-#Aca van los modelos en SQLALCHEMY
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+
+db = SQLAlchemy()
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(200), nullable = False , unique = True )
+    password = db.Column(db.String(200), nullable = False)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(200), nullable = False, unique = True)
+    password = db.Column(db.String(200) , nullable = False)
+    email = db.Column(db.String(200), nullable = False , unique = True)
+    company_name = db.Column(db.String(200))
+    phone = db.Column(db.String(200))
+    firstName = db.Column(db.String(200))
+    lastName = db.Column(db.String(200))
+    rut  = db.Column(db.String(200), nullable = False , unique = True)
+    products = db.relationship('Product', backref='user')
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(200))
+    stock  = db.Column(db.Integer, nullable = False)
+    sold_stock = db.Column(db.Integer, nullable = False)
+    price = db.Column(db.Integer, nullable = False)
+
+
