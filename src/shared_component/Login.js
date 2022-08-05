6d95bc80cf9e3b853bfbba68ed11f79c06301a82
Sussan <sussanaracely@gmail.com>
2022-08-05 14:50:08 -0400

    Registro y Login

--- src/shared_component/Login.js
+++ src/shared_component/Login.js
@@ -1,29 +1,63 @@
-import React from "react";
+import * as React from "react";
 import "../styles/Login.css";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+
+
 
 function Login() {
   return (
-    <div className="container-form sign-in">
-      <form className="formulario">
-        <h2 className="create-account">¡Bienvenido!</h2>
-        <div className="email">
-        <input type="email" placeholder="Email" />
-        </div>
-        <div className="contraseña">
-          <input type="password" placeholder="Contraseña" />
-        </div>
-        <div className="iniciarsesión">
-        <input type="button" defaultValue="Iniciar Sesión" />
-        </div>
-        <div className="pregunta">
+    <div className="Paper">
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 400,
+          height: 400,
+        },
+      }}
+    >
+      <Paper elevation={3} sx={{ maxWidth: 280, maxHeight: 480, marginLeft: 3, margin: 2 }}>
+      
+  
+      <h2 className="account">¡Bienvenido!</h2>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField id="outlined-basic" label="Contraseña" variant="outlined" />
+      </Box>
+      <div className="iniciarsesión">
+        <Stack direction="row" spacing={2}>
+          <Button  style={{ backgroundColor: "#0B1F47", color: "white" }}variant="contained" color="success">
+            Iniciar sesión
+          </Button>
+        </Stack>
+      </div>
+      <div className="pregunta">
         <p className="cuenta-gratis">¿Aún no tienes una cuenta?</p>
-        </div>
-        <div className="registrarse">
-        <input type="button" defaultValue="Registrarse" />
-        </div>
-      </form>
-    </div>
-  );
+      </div>
+      <div className="registrarse">
+        <Stack direction="row" spacing={2}>
+          <Button style={{ backgroundColor: "#0B1F47", color: "white" }}variant="contained" color="success">
+            Registrarse
+          </Button>
+        </Stack>
+      </div >
+      </Paper>
+      </Box>
+      </div>
+      );
 }
 
 export default Login;
--- src/shared_component/Register.js
+++ src/shared_component/Register.js
@@ -1,27 +1,52 @@
-import React from "react";
-import "../styles/Login.css";
+import "../styles/Register.css";
+import * as React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 
 function Register() {
   return (
-    <div className="container-form sign-in">
-      <form className="formulario">
-        <h2 className="create-account">Crear una cuenta</h2>
-        <i className="fa-solid fa-envelope"></i>
-        <div className="email">
-        <input type="email" placeholder="Email" />
-        </div>
-        <div className="contraseña">
-          <input type="password" placeholder="Contraseña" />
-        </div>
-        <div className="iniciarsesión">
-        <input type="button" defaultValue="Iniciar Sesión" />
-        </div>
-        <div className="registrarse">
-        <input type="button" defaultValue="Registrarse" />
-        </div>
-      </form>
-    </div>
-  );
+    <div className="Paper">
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 400,
+          height: 400,
+        },
+      }}
+    >
+      <Paper elevation={3} sx={{ maxWidth: 280, maxHeight: 480, marginLeft: 3, margin: 2 }}>
+      
+  
+      <h2 className="account">Crea una cuenta</h2>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Nombre" variant="outlined" />
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField id="outlined-basic" label="Contraseña" variant="outlined" />
+      </Box>
+      <div className="iniciarsesión">
+        <Stack direction="row" spacing={2}>
+          <Button  style={{ backgroundColor: "#0B1F47", color: "white" }}variant="contained" color="success">
+            Registrarse
+          </Button>
+        </Stack>
+      </div>
+      </Paper>
+      </Box>
+      </div>
+      );
 }
 
 export default Register;
