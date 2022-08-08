288457bcf84dfae4eb5093b4316dedcc379d7773
FelipeV1993 <f.vasquezossa@gmail.com>
2022-08-08 10:53:44 -0400

    fixing error from merge

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Login from "./shared_component/Login";
 import Register from "./admin/Register";
@@ -5,12 +6,18 @@ import Prueba from "./shared_component/prueba";
 
 
 
+import Navbar from "./shared_component/navbar";
+import HomeUser from "./user/home_user";
+
+
 function App() {
   return (
     <>
+
     <Login />
   </>
   )
+
 }
 
 export default App;
