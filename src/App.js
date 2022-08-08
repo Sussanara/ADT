5193af97c17ad21d822482c2977a1f0cc7b3b991
FelipeV1993 <f.vasquezossa@gmail.com>
2022-08-08 18:08:35 -0400

    preparin for merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-
 import React from "react";
 import Login from "./shared_component/Login";
 import Register from "./admin/Register";
-import Crud from './admin/home';
-
-
+import Crud from "./admin/home";
 
 import Navbar from "./shared_component/navbar";
 import HomeUser from "./user/home_user";
 
-
 function App() {
   return (
     <>
-
-    <Crud />
-
-  </>
-  )
-
+      <Navbar />
+      <Login />
+    </>
+  );
 }
 
 export default App;
