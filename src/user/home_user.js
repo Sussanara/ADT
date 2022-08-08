3d101b663d6ffc5e68dbdb5725dffca4ff0da708
Patricio Espinoza <p.alfonsoespinoza@gmail.com>
2022-08-08 10:40:15 -0400

    update modals and data

--- src/user/home_user.js
+++ src/user/home_user.js
@@ -29,7 +29,19 @@ const style = {
 };
 
 export const HomeUser = () => {
-  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const array = [
+    {name:"mayo",stock:"123",sold_stock:"33",price:"2500" },
+    {name:"coca-cola",stock:"77",sold_stock:"25",price:"2000" },
+    {name:"lays",stock:"322",sold_stock:"200",price:"500" },
+    {name:"donuts",stock:"44",sold_stock:"12",price:"900" },
+    {name:"orange",stock:"65",sold_stock:"33",price:"2000" },
+    {name:"bimbo",stock:"22",sold_stock:"11",price:"4500" },
+    {name:"sprite",stock:"83",sold_stock:"32",price:"2000" },
+    {name:"chettos",stock:"287",sold_stock:"22",price:"500" },
+    {name:"chocolitos",stock:"50",sold_stock:"12",price:"600" },
+    {name:"lapiz",stock:"300",sold_stock:"147",price:"100" }
+  ]
+  ;
   const [modalEdit, setModalEdit] = React.useState(false);
 
   const [modalDelete, setModalDelete] = React.useState(false);
@@ -68,7 +80,7 @@ export const HomeUser = () => {
             },
           }}
         >
-          {array.map((numero) => {
+          {array.map((product) => {
             return (
               <Paper
                 elevation={8}
@@ -83,22 +95,27 @@ export const HomeUser = () => {
                     image="https://picsum.photos/200/300"
                     alt=""
                   />
-
+ {/*   id = db.Column(db.Integer, primary_key = True)
+  owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  name = db.Column(db.String(200), nullable = False)
+  stock  = db.Column(db.Integer, nullable = False)
+  sold_stock = db.Column(db.Integer, nullable = False)
+  price = db.Column(db.Integer, nullable = False)  */}
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                      Nombre producto
+                      {product.name}
                     </Typography>
                     <Typography variant="body2" color="text.primary">
-                      Stock: 10
+                      Stock: {product.stock}
                     </Typography>
                     <Typography variant="body2" color="text.primary">
-                      Vendidas: 15
+                      Vendidas: {product.sold_stock}
                     </Typography>
                     <Typography variant="body2" color="text.primary">
-                      valor: $1.000
+                      valor: ${product.price}
                     </Typography>
                     <Typography variant="body2" color="text.primary">
-                      Ganancia: $3.000
+                      Ganancia: ${product.price * product.sold_stock}
                     </Typography>
                   </CardContent>
                   <CardActions
@@ -154,7 +171,7 @@ export const HomeUser = () => {
                   shrink: true,
                 }}
               />
-
+            &nbsp; &nbsp;
           <TextField
                 id="outlined-number"
                 label="Cantidad de Articulos"
@@ -164,7 +181,6 @@ export const HomeUser = () => {
                 }}
               />
               
-             
             </div>
             <div className="buttonAdd">
               <Button
@@ -188,31 +204,75 @@ export const HomeUser = () => {
         </Modal>
 
         <Modal
-          open={modalEdit}
-          onClick={()=> {setModalEdit(false)}}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Modal Editar
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-          </Box>
-        </Modal>
-
-        <Modal
           open={modalDelete}
           
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <div></div>
+            <div>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Modal Eliminar
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+            </div>
+            <div className="buttonSales">
+              <Button
+                style={{ backgroundColor: "#32CD32", color: "white" }}
+                variant="contained"
+                onClick={()=>{setModalDelete(false)}}
+              >
+                Aceptar
+              </Button>
+              &nbsp; &nbsp;
+              <Button
+                style={{ backgroundColor: "#F44336", color: "white" }}
+                variant="contained"
+                onClick={()=>{setModalDelete(false)}}
+              >
+                Cancelar
+              </Button>
+            </div>
           </Box>
         </Modal>
+        
+        <Modal
+          open={modalEdit}
+          
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Modal Editar
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+            </div>
+            <div className="buttonSales">
+              <Button
+                style={{ backgroundColor: "#32CD32", color: "white" }}
+                variant="contained"
+                onClick={()=>{setModalEdit(false)}}
+              >
+                Aceptar
+              </Button>
+              &nbsp; &nbsp;
+              <Button
+                style={{ backgroundColor: "#F44336", color: "white" }}
+                variant="contained"
+                onClick={()=>{setModalEdit(false)}}
+              >
+                Cancelar
+              </Button>
+            </div>
+          </Box>
+        </Modal>
+
 
         <Modal
           open={modalVentas}
